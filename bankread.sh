77960e81aa3f853a9400cdccbd924a5cc2ac4429
WiiPrgm <support@byterenew.com>
2025-12-06 21:53:32 -0600

    Update bankread.sh
    Added logic to check for and output game type (Wii or GC).
    
    TODO:
    Add logic to skip a bank if a DL game is detected.

--- bankread.sh
+++ bankread.sh
@@ -1,7 +1,7 @@
 offset=3145737
 bank=1
 nhcd=1610613248
-
+type=""
 for run in {1..8}; do
   echo "Bank "$bank":"
   ((bank++))
@@ -9,7 +9,17 @@ for run in {1..8}; do
   #printf '\n'
   offset=$((offset+9193984))
 
-  dd if=$1 bs=1 skip=$nhcd count=4 status=none
+  type=$(dd if=$1 bs=1 skip=$nhcd count=4 status=none)
+  if [[ "$type" == "NN1L" ]]; then
+	echo "Single Layer Wii Game"
+  elif [[ "$type" == "GC1L" ]]; then
+	echo "Gamecube Game"
+  elif [[ "$type" == "NN2L" ]]; then
+	echo "Dual Layer Wii Game"
+  else
+	echo "Deleted Bank"
+  fi
+
   nhcd=$((nhcd+512))
   printf '\n'
   printf '\n'
