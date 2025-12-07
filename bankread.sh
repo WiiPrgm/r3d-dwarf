87ccc14b95b787f033c0789e88b9a1122d6485dc
WiiPrgm <support@byterenew.com>
2025-12-06 21:44:56 -0600

    Update bankread.sh
    Added the ability to read from the nhcd header.

--- bankread.sh
+++ bankread.sh
@@ -1,10 +1,16 @@
 offset=3145737
 bank=1
+nhcd=1610613248
 
 for run in {1..8}; do
   echo "Bank "$bank":"
   ((bank++))
   dd if=$1 bs=512 skip=$offset count=1 status=none | strings
+  #printf '\n'
+  offset=$((offset+9193984))
+
+  dd if=$1 bs=1 skip=$nhcd count=4 status=none
+  nhcd=$((nhcd+512))
   printf '\n'
-offset=$((offset+9193984))
+  printf '\n'
 done
