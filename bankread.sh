59be40d3ec1c08847fcc3119b381e97c72889ca5
WiiPrgm <support@byterenew.com>
2025-12-07 13:23:58 -0600

    Update bankread.sh
    Added logic to skip the next bank when a dual layer game is detected.
    
    This will probably not work if there's a deleted dual layer game.

--- bankread.sh
+++ bankread.sh
@@ -3,21 +3,24 @@ bank=1
 nhcd=1610613248
 type=""
 
-for run in {1..8}; do
+while (( bank <= 8 )); do
   echo "Bank "$bank":"
   ((bank++))
   dd if=$1 bs=512 skip=$offset count=1 status=none | strings
   #printf '\n'
   offset=$((offset+9193984))
 
-  type=$(dd if=$1 bs=1 skip=$nhcd count=4 status=none)
+  type=$(dd if=$1 bs=1 skip=$nhcd count=4 status=none | tr -d '\0\n')
   if [[ "$type" == "NN1L" ]]; then
 	echo "Single Layer Wii Game"
   elif [[ "$type" == "GC1L" ]]; then
 	echo "Gamecube Game"
   elif [[ "$type" == "NN2L" ]]; then
 	echo "Dual Layer Wii Game"
-  else
+	offset=$((offset+9193984))
+	((bank++))
+	nhcd=$((nhcd+512))
+else
 	echo "Deleted Bank"
   fi
 
@@ -25,4 +28,3 @@ for run in {1..8}; do
   printf '\n'
   printf '\n'
 done
-
